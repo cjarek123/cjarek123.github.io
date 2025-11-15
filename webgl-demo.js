44629075bed63ddb6fa2384ce75e3e5b01d1b5ce
cjarek123 <cjarek123@gmail.com>
2025-11-15 16:42:19 -0700

    6.1

--- webgl-demo.js
+++ webgl-demo.js
@@ -31,24 +31,20 @@ function main() {
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
-    //varying lowp vec4 vColor;
     varying highp vec2 vTextureCoord;
 
     void main(void) {
       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-      //vColor = aVertexColor;
       vTextureCoord = aTextureCoord;
     }
   `;
   // Fragment shader program
   const fsSource = `
-    //varying lowp vec4 vColor;
     varying highp vec2 vTextureCoord;
 
     uniform sampler2D uSampler;
 
     void main(void) {
-      //gl_FragColor = vColor;
       gl_FragColor = texture2D(uSampler, vTextureCoord);
     }
   `;
