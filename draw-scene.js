079e415c3d8a7fb1e34018971cfe2c6f6fc4ab78
cjarek123 <cjarek123@gmail.com>
2025-11-15 17:23:48 -0700

    lighting in webgl

--- draw-scene.js
+++ draw-scene.js
@@ -56,11 +56,15 @@ function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
     [1, 0, 0]
   ); // axis to rotate around (X)
 
+  const normalMatrix = mat4.create();
+  mat4.invert(normalMatrix, modelMatrix);
+  mat4.transpose(normalMatrix, normalMatrix);
+
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   setPositionAttribute(gl, buffers, programInfo);
-
   setTextureAttribute(gl, buffers, programInfo);
+  setNormalAttribute(gl, buffers, programInfo);
 
   // Tell WebGL which indices to use to index the vertices
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
@@ -78,6 +82,11 @@ function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
     programInfo.uniformLocations.modelViewMatrix,
     false,
     modelViewMatrix
+  );
+  gl.uniformMatrix4fv(
+    programInfo.uniformLocations.normalMatrix,
+    false,
+    normalMatrix
   );
 
   // Tell WebGL we want to affect texture unit 0
@@ -155,6 +164,26 @@ function setTextureAttribute(gl, buffers, programInfo) {
     offset
   );
   gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+}
+
+// Tell WebGL how to pull out the normals from
+// the normal buffer into the vertexNormal attribute.
+function setNormalAttribute(gl, buffers, programInfo) {
+  const numComponents = 3;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  const offset = 0;
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
+  gl.vertexAttribPointer(
+    programInfo.attribLocations.vertexNormal,
+    numComponents,
+    type,
+    normalize,
+    stride,
+    offset,
+  );
+  gl.enableVertexAttribArray(programInfo.attribLocations.vertexNormal);
 }
 
 export { drawScene };
