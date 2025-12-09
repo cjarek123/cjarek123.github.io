4511aa34f3cf3696f20064e96e4551451f8654ce
ARUIZV0322 <adrianrv0322@gmail.com>
2025-12-09 15:05:27 -0700

    adding rod prmitives

--- combined/entities/fishingrod.js
+++ combined/entities/fishingrod.js
@@ -52,70 +52,152 @@ class FishingRod extends Entity {
         this.startTime = 0.0;
 
         // primitives
-        let grass = new RectPrism(color, new Vec3(scale_factor*400.0, scale_factor*100.0, scale_factor*4.0), MATERIALS.DIFFUSE, 0.0, 0.0);
+        let rodBase = new Cylinder(color, scale_factor*0.2, scale_factor*3.0, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let rodArm = new Cylinder(color, scale_factor*0.1, scale_factor*10.0, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let coreCylinder = new Cylinder(color, scale_factor*0.5, scale_factor*1.0, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let coreSphere = new Ellipsoid(color, new Vec3(scale_factor*0.5, scale_factor*0.5, scale_factor*0.5), MATERIALS.DIFFUSE, 0.0, 0.0);
+        let handleBase = new Cylinder(color, scale_factor*0.1, scale_factor*0.5, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let handleArm = new Cylinder(color, scale_factor*0.1, scale_factor*0.5, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let handleGrip = new Cylinder(color, scale_factor*0.1, scale_factor*0.5, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let ring1 = new Torus(color, scale_factor*0.2, scale_factor*0.03, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let ring2 = new Torus(color, scale_factor*0.2, scale_factor*0.03, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let ring3 = new Torus(color, scale_factor*0.2, scale_factor*0.03, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let ring4 = new Torus(color, scale_factor*0.2, scale_factor*0.03, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let ring5 = new Torus(color, scale_factor*0.2, scale_factor*0.03, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let stringA = new Cylinder(color, scale_factor*0.01, scale_factor*2.0, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let stringB = new Cylinder(color, scale_factor*0.01, scale_factor*8.0, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let stringC = new Cylinder(color, scale_factor*0.01, scale_factor*1.0, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let bobberSphere = new Ellipsoid(color, new Vec3(scale_factor*0.2, scale_factor*0.2, scale_factor*0.2), MATERIALS.DIFFUSE, 0.0, 0.0);
+        let bobberHead = new Cylinder(color, scale_factor*0.075, scale_factor*0.05, MATERIALS.DIFFUSE, 0.0, 0.0);
+        let bobberRing = new Torus(color, scale_factor*0.2, scale_factor*0.02, MATERIALS.DIFFUSE, 0.0, 0.0);
 
         // nodes
-        this.grassNode = new Node(grass);
+        this.rodBaseNode = new Node(rodBase);
+        this.rodArmNode = new Node(rodArm);
+        this.coreCylinderNode = new Node(coreCylinder);
+        this.coreSphereNode = new Node(coreSphere);
+        this.handleBaseNode = new Node(handleBase);
+        this.handleArmNode = new Node(handleArm);
+        this.handleGripNode = new Node(handleGrip);
+        this.ring1Node = new Node(ring1);
+        this.ring2Node = new Node(ring2);
+        this.ring3Node = new Node(ring3);
+        this.ring4Node = new Node(ring4);
+        this.ring5Node = new Node(ring5);
+        this.stringANode = new Node(stringA);
+        this.stringBNode = new Node(stringB);
+        this.stringCNode = new Node(stringC);
+        this.bobberSphereNode = new Node(bobberSphere);
+        this.bobberHeadNode = new Node(bobberHead);
+        this.bobberRingNode = new Node(bobberRing);
 
         // tranformations
+        this.handleGripNode.setParent(this.handleArmNode);
+        this.handleArmNode.setParent(this.handleBaseNode);
+        this.stringANode.setParent(this.coreSphereNode);
+        this.stringBNode.setParent(this.stringANode);
+        this.stringCNode.setParent(this.stringBNode);
+        this.coreSphereNode.setParent(this.coreCylinderNode);
+        this.handleBaseNode.setParent(this.coreCylinderNode);
+        this.ring1Node.setParent(this.rodArmNode);
+        this.ring2Node.setParent(this.rodArmNode);
+        this.ring3Node.setParent(this.rodArmNode);
+        this.ring4Node.setParent(this.rodArmNode);
+        this.ring5Node.setParent(this.rodArmNode);
+        this.coreCylinderNode.setParent(this.rodBaseNode);
+        this.rodArmNode.setParent(this.rodBaseNode);    
+        this.bobberHeadNode.setParent(this.stringBNode);
+        this.bobberSphereNode.setParent(this.bobberHeadNode);
+        this.bobberRingNode.setParent(this.bobberSphereNode);
 
-        // translate grass block
-        this.grassNode.localMatrix.translate(center.x, center.y, center.z);
+        // rod base
+        this.rodBaseNode.localMatrix.translate(center.x, center.y, center.z);
+        this.rodBaseNode.localMatrix.rotateX(Math.PI/16);
+        // rod arm
+        this.rodArmNode.localMatrix.translate(0.0, 0.0, scale_factor*3.5);
+        // rings
+        this.ring1Node.localMatrix.translate(0.0, scale_factor*0.25, scale_factor*1.0);
+        this.ring2Node.localMatrix.translate(0.0, scale_factor*0.25, scale_factor*2.0);
+        this.ring3Node.localMatrix.translate(0.0, scale_factor*0.25, scale_factor*3.0);
+        this.ring4Node.localMatrix.translate(0.0, scale_factor*0.25, scale_factor*4.0);
+        this.ring5Node.localMatrix.translate(0.0, scale_factor*0.25, scale_factor*5.0);
+        // strings
+        // this.stringANode.localMatrix.rotateX(Math.PI/15);
+        // this.stringANode.localMatrix.translate(0.0, 0.0, scale_factor*0.5);
+        // this.stringBNode.localMatrix.rotateX(-Math.PI/15);
+        // this.stringBNode.localMatrix.translate(0.0, 0.0, scale_factor*4.0);
 
         // update nodes world matrices (need to only call on root nodes)
-        this.grassNode.updateWorldMatrix();
+        this.rodBaseNode.updateWorldMatrix();
 
         this.nodes = [
-            // this.grassNode
+            this.rodBaseNode,
+            this.rodArmNode,
+            // this.coreCylinderNode,
+            // this.coreSphereNode,
+            // this.handleBaseNode,
+            // this.handleArmNode,
+            // this.handleGripNode,
+            this.ring1Node,
+            this.ring2Node,
+            this.ring3Node,
+            this.ring4Node,
+            this.ring5Node,
+            // this.stringANode,
+            // this.stringBNode,
+            // this.stringCNode,
+            // this.bobberSphereNode,
+            // this.bobberHeadNode,
+            // this.bobberRingNode
         ]
 
     }
 
     animate(deltaTime) {
-        switch(this.state){
-            case fishingRod.IDLE:
-                console.log("IDLE");
-                this.bobberX = 0.0;
-                this.bobberY = 0.0;
-                this.bobberZ = 0.0;
-                this.charge = 0.0;
-                break;
+        // switch(this.state){
+        //     case FishingRod.IDLE:
+        //         console.log("IDLE");
+        //         this.bobberX = 0.0;
+        //         this.bobberY = 0.0;
+        //         this.bobberZ = 0.0;
+        //         this.charge = 0.0;
+        //         break;
                 
-            case fishingRod.CHARGING:
-                console.log("CHARGING");
-                this.bobberX = 0.0;
-                this.bobberY = 0.0;
-                this.bobberZ = 0.0;
-                this.startTime = Date.now();
-                break;
+        //     case FishingRod.CHARGING:
+        //         console.log("CHARGING");
+        //         this.bobberX = 0.0;
+        //         this.bobberY = 0.0;
+        //         this.bobberZ = 0.0;
+        //         this.startTime = Date.now();
+        //         break;
 
-            case fishingRod.RELEASING:
-                console.log("RELEASING");
-                let dt = (Date.now() - this.startTime)*0.001;
+        //     case FishingRod.RELEASING:
+        //         console.log("RELEASING");
+        //         let dt = (Date.now() - this.startTime)*0.001;
 
-                //this.bobberX = this.charge*this.launchSpdX*dt;
-                this.bobberY = this.charge*this.launchSpdY*dt + 0.5*this.g*dt*dt;
-                this.bobberZ = this.charge*this.launchSpdZ*dt;
+        //         //this.bobberX = this.charge*this.launchSpdX*dt;
+        //         this.bobberY = this.charge*this.launchSpdY*dt + 0.5*this.g*dt*dt;
+        //         this.bobberZ = this.charge*this.launchSpdZ*dt;
 
-                if(this.bobberY <= -13.0){
-                this.bobberY = -13.0;
-                this.state = fishingRod.FISHING;
-                }
-                break;
+        //         if(this.bobberY <= -13.0){
+        //         this.bobberY = -13.0;
+        //         this.state = fishingRod.FISHING;
+        //         }
+        //         break;
 
-            case fishingRod.FISHING:
-                console.log("FISHING");
-                this.bobberY = -13.0+Math.sin(Date.now()*0.005)*0.1;
-                break;
+        //     case FishingRod.FISHING:
+        //         console.log("FISHING");
+        //         this.bobberY = -13.0+Math.sin(Date.now()*0.005)*0.1;
+        //         break;
 
-            case fishingRod.REELING:
-                console.log("REELING");
-                this.bobberZ += fishingRod.REELSPEED;
-                if(this.bobberZ >= -1.5){
-                    this.bobberY = 0.0;
-                    this.state = fishingRod.IDLE;
-                }
-                break;
-        }
+        //     case FishingRod.REELING:
+        //         console.log("REELING");
+        //         this.bobberZ += fishingRod.REELSPEED;
+        //         if(this.bobberZ >= -1.5){
+        //             this.bobberY = 0.0;
+        //             this.state = fishingRod.IDLE;
+        //         }
+        //         break;
+        // }
     }
 }
